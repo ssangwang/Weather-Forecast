25cc09fb837f38e0a90d603ab75ee0803553c7f9
Code <sangw009@umn.edu>
2022-10-10 13:08:30 -0500

    added five day forecast, need to edit css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,9 @@
-var apiKey = "https://api.openweathermap.org/data/2.5/forecast?q=sacramento&cnt=5&appid=783819609a1154fecc0d013aa520cde6"
-//check to see if url works
-function getapi() {
-    fetch(apiKey)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-}
 
-getapi();
 //set search into local storage. 
 function getCity(){
     var cityInput = document.getElementById('srchBx');
     localStorage.setItem("City", cityInput.value);
-    var srchCity = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&cnt=5&appid=783819609a1154fecc0d013aa520cde6&units=imperial";
+    var srchCity = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&cnt=6&appid=783819609a1154fecc0d013aa520cde6&units=imperial";
     fetch(srchCity)
     .then(function (response) {
         return response.json();
@@ -26,6 +14,23 @@ function getCity(){
         document.getElementById('crntTemp').textContent = "Temperature: " + data.list[0].main.temp + "℉";
         document.getElementById('crntWind').textContent = "Wind: " + data.list[0].wind.speed + "mph";
         document.getElementById('crntHumid').textContent ="Humidity: " + data.list[0].main.humidity + "%";
+        // set forecast 
+        for(i = 1; i<data.list.length; i++) {
+            var forecastTemp = document.createElement('li');
+            var forecastWind = document.createElement('li');
+            var forecastHumid = document.createElement('li');
+            var forecastDate = document.createElement('h3')
+            var forecastBox = document.getElementById('5day');
+            forecastDate.textContent = "Date";
+            forecastTemp.textContent = "Temperature: " + data.list[i].main.temp + "℉";
+            forecastWind.textContent = "Wind: " + data.list[i].wind.speed + "mph";
+            forecastHumid.textContent ="Humidity: " + data.list[i].main.humidity + "%";
+            forecastBox.append(forecastDate);
+            forecastDate.append(forecastHumid);
+            forecastDate.append(forecastTemp);
+            forecastDate.append(forecastWind);
+        }
+
     });
 
     // input data into boxes 
