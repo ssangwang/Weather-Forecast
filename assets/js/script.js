d5f85c286ede86bac8fccc834ba96df892928ada
Code <sangw009@umn.edu>
2022-10-10 11:17:39 -0500

    inserted JSON file into HTML" sdsd - - m ` git

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,18 +15,25 @@ getapi();
 function getCity(){
     var cityInput = document.getElementById('srchBx');
     localStorage.setItem("City", cityInput.value);
-    var srchCity = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=783819609a1154fecc0d013aa520cde6";
+    var srchCity = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=783819609a1154fecc0d013aa520cde6&units=imperial";
     fetch(srchCity)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
+        document.getElementById('crntTemp').textContent = "Temperature: " + data.main.temp + "℉";
+        document.getElementById('crntWind').textContent = "Wind: " + data.wind.speed + "mph";
+        document.getElementById('crntHumid').textContent ="Humidity: " + data.main.humidity + "%";
     });
+
     // input data into boxes 
     document.getElementById('currentCity').textContent = (cityInput.value);
+    document.getElementById('recent1').textContent = cityInput.value;
+
 }
 
+// search button
 var srchBtn = document.getElementById('srchBtn');
 
 srchBtn.addEventListener("click", getCity);
