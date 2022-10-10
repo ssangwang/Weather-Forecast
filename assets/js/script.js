8625da4f7799f296ec3fd054bc07f26181f0494e
Code <sangw009@umn.edu>
2022-10-10 10:25:16 -0500

    created function to set searched cities in local storage

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+var apiKey = "https://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=783819609a1154fecc0d013aa520cde6"
+//check to see if url works
+function getapi() {
+    fetch(apiKey)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+getapi();
+//set search into local storage. 
+function getCity(){
+    var cityInput = document.getElementById('srchBx');
+    localStorage.setItem("City", cityInput.value);
+    var srchCity = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=783819609a1154fecc0d013aa520cde6";
+    fetch(srchCity)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+    // input data into boxes 
+    document.getElementById('crntCity').textContent(cityInput.value);
+}
+
+var srchBtn = document.getElementById('srchBtn');
+
+srchBtn.addEventListener("click", getCity);
+
+
+
+
